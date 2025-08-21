da7b44b9f7ebe213b1da311db36625ee747de3d5
Dev-KimDonghyun <hyungus723@gmail.com>
2025-08-21 23:07:41 +0900

    chore: Add Example Database Schema

---
+++ init_DB/001_schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  local VARCHAR(100) NOT NULL,
+  age INT
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users(name, local, age)
+VALUES
+    ('Kim', 'Daegu, Republic of Korea', 19),
+    ('Takahashi', 'Saitama, Japan', 21),
+    ('John', 'Kentucky, United States of America', 20);
